c8d401e14cdfa29e590c31999677563a8a7731fc
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2023-12-14 09:50:30 +0000

    Outage Feed - de-duplicate Embed & Button creation

--- BotModules/OutageFeedModule.js
+++ BotModules/OutageFeedModule.js
@@ -16,6 +16,20 @@ module.exports = {
 
         // Fetch all DB entries
         let allDbEntries = await OutageFeedModel.find();
+
+        // Create Embed
+        const OutageEmbed = new EmbedBuilder()
+        .setColor(incident.impact === "none" ? Colors.Default : incident.impact === "minor" ? '#13b307' : incident.impact === "major" ? '#e8e409' : '#940707')
+        .setTitle(incident.name)
+        .setDescription(`Impact: ${incident.impact}`)
+        .addFields(incident.incident_updates.reverse().map(incidentUpdate => { return { name: `${incidentUpdate.status.charAt(0).toUpperCase() + incidentUpdate.status.slice(1)} ( <t:${Math.floor(incidentUpdate.updated_at.getTime() / 1000)}:R> )`, value: (incidentUpdate.body || "No information available.") } }).slice(-5))
+        .setFooter({ text: `Posted by ${DiscordClient.user.username}#${DiscordClient.user.discriminator} ` })
+        .setTimestamp(incident.created_at);
+
+        // Create URL button
+        const OutagePageLinkButton = new ActionRowBuilder().addComponents([
+            new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel("View Status Page").setURL(incident.shortlink)
+        ]);
         
         
         // Is this a new Outage, or an update to an ongoing one?
@@ -23,19 +37,6 @@ module.exports = {
         if ( !OutageCollection )
         {
             // New Outage
-            // Create new Embed
-            const OutageEmbedNew = new EmbedBuilder()
-            .setColor(incident.impact === "none" ? Colors.Default : incident.impact === "minor" ? '#13b307' : incident.impact === "major" ? '#e8e409' : '#940707')
-            .setTitle(incident.name)
-            .setDescription(`Impact: ${incident.impact}`)
-            .addFields(incident.incident_updates.reverse().map(incidentUpdate => { return { name: `${incidentUpdate.status.charAt(0).toUpperCase() + incidentUpdate.status.slice(1)} ( <t:${Math.floor(incidentUpdate.updated_at.getTime() / 1000)}:R> )`, value: (incidentUpdate.body || "No information available.") } }).slice(-5))
-            .setFooter({ text: `Posted by ${DiscordClient.user.username}#${DiscordClient.user.discriminator} ` })
-            .setTimestamp(incident.created_at);
-
-            // Create URL button
-            const OutagePageLinkButton = new ActionRowBuilder().addComponents([
-                new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel("View Status Page").setURL(incident.shortlink)
-            ]);
 
             // Fetch webhooks, send message via said webhooks, then store sent message IDs for editing later updates into
             const SentMessageCollection = new Collection();
@@ -48,7 +49,7 @@ module.exports = {
                         allowedMentions: { parse: [] },
                         threadId: document.threadId instanceof String ? document.threadId : null,
                         content: `**Discord Outage:**`,
-                        embeds: [OutageEmbedNew],
+                        embeds: [OutageEmbed],
                         components: [OutagePageLinkButton]
                     })
                     .then(sentMessage => { SentMessageCollection.set(fetchedWebhook.id, sentMessage.id); })
@@ -69,20 +70,7 @@ module.exports = {
         else
         {
             // Ongoing Outage
-            // Create updated Embed
-            const OutageEmbedUpdate = new EmbedBuilder()
-            .setColor(incident.impact === "none" ? Colors.Default : incident.impact === "minor" ? '#13b307' : incident.impact === "major" ? '#e8e409' : '#940707')
-            .setTitle(incident.name)
-            .setDescription(`Impact: ${incident.impact}`)
-            .addFields(incident.incident_updates.reverse().map(incidentUpdate => { return { name: `${incidentUpdate.status.charAt(0).toUpperCase() + incidentUpdate.status.slice(1)} ( <t:${Math.floor(incidentUpdate.updated_at.getTime() / 1000)}:R> )`, value: (incidentUpdate.body || "No information available.") } }).slice(-5))
-            .setFooter({ text: `Posted by ${DiscordClient.user.username}#${DiscordClient.user.discriminator} ` })
-            .setTimestamp(incident.created_at);
-
-            // Create URL button
-            const OutagePageLinkButton = new ActionRowBuilder().addComponents([
-                new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel("View Status Page").setURL(incident.shortlink)
-            ]);
-
+            
             // Fetch webhooks, edit messages via said webhooks
             allDbEntries.forEach(async document => {
                 // Fetch webhook
@@ -90,7 +78,7 @@ module.exports = {
                 .then(async fetchedWebhook => {
                     await fetchedWebhook.editMessage(OutageCollection.get(fetchedWebhook.id), {
                         allowedMentions: { parse: [] },
-                        embeds: [OutageEmbedUpdate],
+                        embeds: [OutageEmbed],
                         components: [OutagePageLinkButton]
                     })
                     .catch(async err => {
