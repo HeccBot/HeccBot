261ae4affa6fb756cbb5300b7fd0cb43c38d804b
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2024-05-14 13:52:59 +0100

    DAMMIT

--- Interactions/SlashCommands/HeccBot/news.js
+++ Interactions/SlashCommands/HeccBot/news.js
@@ -6,7 +6,7 @@ const { LogToUserInteraction } = require("../../../BotModules/LoggingModule");
 module.exports = {
     // Command's Name
     //     Use full lowercase
-    Name: "heccbot",
+    Name: "news",
 
     // Command's Description
     Description: `Subscribe this Server to HeccBot's Announcement Feed`,
